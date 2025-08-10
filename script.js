bb40715c5812c20f557034f0f93d2bfccfcfcdc3
Blue Ocean <106617684+judenns@users.noreply.github.com>
2025-08-10 17:55:45 +0700

    update bg image + fix input

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const category = document.querySelector('input[name="category"]:checked').value;
 // Các element UI
 const btnAdd = document.getElementById("btn-add");
 const btnSave = document.getElementById("btn-save");
-const addExpenseCard = document.getElementById("add-expense-card");
-const expensesList = document.getElementById("expenses-list");
-const overallCard = document.getElementById("overall-expense-card");
+const cardAdd = document.getElementById("add-expense-card");
+const cardList = document.getElementById("expenses-list");
+const cardOverall = document.getElementById("overall-expense-card");
+
+btnAdd.addEventListener("click", function () {
+  cardAdd.classList.remove("card-hidden");
+  cardOverall.classList.add("card-hidden");
+});
+
+btnSave.addEventListener("click", function (e) {
+  e.preventDefault();
+});
